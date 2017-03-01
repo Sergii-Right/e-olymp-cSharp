7da1ad22027670d9e48d903610b7cbac31f79386
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-01 16:32:51 +0200

    Test .gitignore

---
+++ Supply_of_soda_542/Supply_of_soda_542/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supply_of_soda_542
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
