c0efe14ff7be1bb520f293e968abaa36a58dcdc5
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-02 12:15:24 +0200

    Units 622

---
+++ Units_622/Units_622/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Units_622
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var lst = new List<int>();
+            for (;;)
+            {
+                lst.Add(n % 2);
+                n /= 2;
+                if(n == 0) break;
+            }
+
+            Console.WriteLine(lst.Count(c => c == 1));
+        }
+    }
+}
