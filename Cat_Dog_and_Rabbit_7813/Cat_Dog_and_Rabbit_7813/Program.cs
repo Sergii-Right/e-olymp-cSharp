45ac53b5c49bb5d077bc64fd221afad6f0b288a6
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-04 21:17:59 +0200

    Cat dog and rabbit 7813

---
+++ Cat_Dog_and_Rabbit_7813/Cat_Dog_and_Rabbit_7813/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cat_Dog_and_Rabbit_7813
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split(' ');
+            var a = int.Parse(s[0]);
+            var b = int.Parse(s[1]);
+            var c = int.Parse(s[2]);
+
+            var z = (c + b - a) / 2.0;
+            var y = b - z;
+            var x = a - y;
+
+            Console.WriteLine("{0:f2} {1:f2} {2:f2}", x, y, z);
+        }
+    }
+}
