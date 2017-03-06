c4042c7374213068f18cac859dfd57655c690a20
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-06 15:54:31 +0200

    7368

---
+++ Avarage_of_scaters_7368/Avarage_of_scaters_7368/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Avarage_of_scaters_7368
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            List<double> lst = new List<double>();
+            for (var i = 0; i < s[1]; i++)
+            {
+                var l = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                lst.Add(l.Where(c => c != l.Max() && c != l.Min()).Average());
+            }
+
+            foreach (var item in lst)
+            {
+                Console.Write("{0:f2} ", item);
+            }
+        }
+    }
+}
---
+++ Avarage_of_scaters_7368/Avarage_of_scaters_7368/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Avarage_of_scaters_7368")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Avarage_of_scaters_7368")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("D751B584-3399-41C3-8B4E-45D5B552A0DD")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
