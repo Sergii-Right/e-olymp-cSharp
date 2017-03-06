b2270d28765e4b4dd1bf6922f32a87e28f9f8f04
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-06 15:21:25 +0200

    7367

---
+++ Sportsman_7367/Sportsman_7367/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sportsman_7367
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var count = 0;
+            double sum = 0;
+            for (double i = 10; sum < n; i += i * 0.1)
+            {
+                sum += i;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
