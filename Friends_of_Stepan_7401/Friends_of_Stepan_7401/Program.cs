e4f19bf58e938ecb451ca1e726fd4e92e4b39b47
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-06 23:30:04 +0200

    7401

---
+++ Friends_of_Stepan_7401/Friends_of_Stepan_7401/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Friends_of_Stepan_7401
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n - 1);
+        }
+    }
+}
