377816760758bd3336cde6f7e12105c31336bfad
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-06 14:04:01 +0200

    7671

---
+++ Chess_olympiad_7671/Chess_olympiad_7671/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chess_olympiad_7671
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = double.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Round(n / 3) + 1);
+        }
+    }
+}
