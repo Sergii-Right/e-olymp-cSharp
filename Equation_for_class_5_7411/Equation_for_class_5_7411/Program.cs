300187d99b9de180eb481a7363c5e662e1be0a5f
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-07 01:03:06 +0200

    7411

---
+++ Equation_for_class_5_7411/Equation_for_class_5_7411/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Equation_for_class_5_7411
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            int x = 0;
+            if (str[1] == '+')
+            {
+                if (str[0] == 'x')
+                {
+                    x = int.Parse(str[4].ToString()) - int.Parse(str[2].ToString());
+                }
+                else if (str[2] == 'x')
+                {
+                    x = int.Parse(str[4].ToString()) - int.Parse(str[0].ToString());
+                }
+                else if(str[4] == 'x')
+                {
+                    x = int.Parse(str[0].ToString()) + int.Parse(str[2].ToString());
+                }
+            }
+            else
+            {
+                if (str[0] == 'x')
+                {
+                    x = int.Parse(str[2].ToString()) + int.Parse(str[4].ToString());
+                }
+                else if (str[2] == 'x')
+                {
+                    x = int.Parse(str[0].ToString()) - int.Parse(str[4].ToString());
+                }
+                else if(str[4] == 'x')
+                {
+                    x = int.Parse(str[0].ToString()) - int.Parse(str[2].ToString());
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
