bf1a99bb1a1a455b6d06a638967ab7d58e118fcd
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-07 09:01:55 +0200

    7326

---
+++ Sleeping_cars_7326/Sleeping_cars_7326/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sleeping_cars_7326
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            int st = 0, count = 0;
+            foreach (var ch in str)
+            {
+                if (ch == 'k')
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+                if (count > st) st = count;
+            }
+            Console.WriteLine(st);
+        }
+    }
+}
