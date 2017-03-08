d3b03d083aa00a1f4a055832aa8ccb581e352366
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-08 21:08:23 +0200

    7293

---
+++ Traffic_rules_7293/Traffic_rules_7293/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Traffic_rules_7293
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(145 - n);
+        }
+    }
+}
