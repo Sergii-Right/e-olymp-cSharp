36209daae962cd9ee8537bdbc03a2895b4eed68b
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-08 21:22:14 +0200

    7290

---
+++ Lazy_student_7290/Lazy_student_7290/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Lazy_student_7290
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            var count = str.Count(c => c == 'a' || c == 'e' || c == 'o' || c == 'g' || c == 'p' || c == 'q' || c == 'b' || c == 'd');
+            Console.WriteLine(count);
+        }
+    }
+}
