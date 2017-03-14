1019a2f7d3a78c9737f418d4e91df8600bc05ba2
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-14 11:13:13 +0200

    6940

---
+++ Maximum_frequency_of_words_6940/Maximum_frequency_of_words_6940/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maximum_frequency_of_words_6940
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, int>();
+            for (var i = 0; i < n; i++)
+            {
+                var str = Console.ReadLine();
+                if (dict.ContainsKey(str))
+                {
+                    dict[str]++;
+                }
+                else
+                {
+                    dict.Add(str, 1);
+                }
+            }
+            var lst = dict.Where(c => c.Value == dict.Values.Max()).ToList();
+            Console.WriteLine($"{lst.Max(c => c.Key)} {lst.First().Value}");
+        }
+    }
+}
