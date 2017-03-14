a1949dbd622eff3bca6b20d6fa78b732c91621db
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-14 09:47:11 +0200

    7108

---
+++ Fire_control_7108/Fire_control_7108/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fire_control_7108
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var num = Console.ReadLine();
+            var str = "";
+            if (num != null)
+                foreach (var c in num)
+                {
+                    if (int.Parse(c.ToString()) == 0 || int.Parse(c.ToString()) == 1)
+                    {
+                        str += c;
+                    }
+                    for (var j = 0; j < int.Parse(c.ToString()); j++)
+                    {
+                        str += c;
+                    }
+                }
+            Console.WriteLine(str);
+        }
+    }
+}
