df7593314140666d6e0d59686a3e37e2735b042e
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-16 13:04:35 +0200

    6413

---
+++ How_6413/How_6413/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace How_6413
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var t = 0;
+            var lst = new List<int>();
+            int.TryParse(Console.ReadLine(), out t);
+            for (var i = 0; i < t; i++)
+            {
+                int sum = 0, n = 0;
+                int.TryParse(Console.ReadLine(), out sum);
+                int.TryParse(Console.ReadLine(), out n);
+                for (var j = 0; j < n; j++)
+                {
+                    var arr = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                    sum += arr[0] * arr[1];
+                }
+                lst.Add(sum);
+            }
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
