b226c2d6c244e74fd0374d706e251d1bccf6c7a7
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-20 13:14:54 +0200

    6412

---
+++ Original_messages_6412/Original_messages_6412/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Original_messages_6412
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<char, int>();
+            for (var i = 0; i < n; i++)
+            {
+                var str = Console.ReadLine();
+                var check = true;
+                if (str == null) continue;
+                long len = str.Length;
+                for (var j = 0; j < len; j++)
+                {
+                    if (!dict.ContainsKey(str[j]))
+                    {
+                        dict.Add(str[j], 1);
+                    }
+                    else
+                    {
+                        dict[str[j]]++;
+                        if (dict[str[j]] != 3) continue;
+                        if (j < len - 1)
+                        {
+                            if (str[j + 1] != str[j])
+                            {
+                                check = false;
+                                break;
+                            }
+                            j++;
+                        }
+                        else
+                        {
+                            check = false;
+                            break;
+                        }
+                        dict[str[j]] = 1;
+                    }
+                }
+                dict.Clear();
+                Console.WriteLine(check ? "OK" : "FAKE");
+            }
+        }
+    }
+}
