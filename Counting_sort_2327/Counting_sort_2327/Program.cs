558e9dfa898a57de242ea9ec43c47823ab9aaa80
Sergii-Right <sergii.ziborov@gmail.com>
2017-03-31 00:18:55 +0300

    2327

---
+++ Counting_sort_2327/Counting_sort_2327/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Counting_sort_2327
+{
+    internal class Program
+    {
+        public static void CountingSort(int[]a, Dictionary<int, int> dict)
+        {
+            foreach (var i in a)
+            {
+                dict[i]++;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            var a = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var dict = new Dictionary<int, int>();
+            for (var i = 0; i <= 100000; i++)
+            {
+                dict.Add(i, 0);
+            }
+            CountingSort(a, dict);
+            var count = 0;
+            foreach (var item in dict)
+            {
+                for (var j = 0; j < item.Value; j++)
+                {
+//                    a[count++] = item.Key;
+                    Console.Write(item.Key);
+                    if(count++ < n - 1) Console.Write(' ');
+                }
+            }
+//            for (var i = 0; i < a.Length; i++)
+//            {
+//                Console.Write(a[i]);
+//                if (i < a.Length - 1) Console.Write(" ");
+//            }
+
+            Console.WriteLine();
+        }
+    }
+}
